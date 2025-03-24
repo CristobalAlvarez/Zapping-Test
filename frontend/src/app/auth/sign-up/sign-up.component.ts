bb8f6c7752c580d0baf1c4580166131283cc4e88
Cristobal Alvarez <ciag2016@gmail.com>
2025-03-23 23:16:21 -0300

    feat: add success message for account creation in login component

--- frontend/src/app/auth/sign-up/sign-up.component.ts
+++ frontend/src/app/auth/sign-up/sign-up.component.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:98082dbedd94e856f6df41b5fc659f77a0f1ea69259ed5bc1de1a5a6e8ddf76f
-size 2286
+oid sha256:b76844ca1f31cadd1d2f4d634f804dd18412320a6ad60cd21f35152eb4c61dcd
+size 2340
